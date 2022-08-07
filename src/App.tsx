36090775806cb8251fe3904f754a7cecae2814bb
DhruvGarg9136 <neelanshugarg097@gmail.com>
2022-08-07 20:12:28 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import { BrowserRouter as Router, Routes,Route, Link } from 'react-router-dom';
 import Home from './layout/Home'
 import Publications from './layout/Publications';
